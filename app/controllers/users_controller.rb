f22fe6e3889e662799cc62ca86bd94fe3a1c24f9
Dharshan Sidharth <dharshan.sidharth@ia.ooo>
2026-07-17 15:17:21 +0530

    git lesson

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    allow_unauthenticated_access only: [:new , :create]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            start_new_session_for(@user)
+            redirect_to root_path
+
+        else
+            flash[:alert] = 'something went wrong!'
+            render :new 
+        end
+    end
+
+    private 
+    def user_params()
+        params.require(:user).permit(:email_address , :password , :password_confirmation)
+    end
+
+end
